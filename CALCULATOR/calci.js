724f274dd43c05204b34939aea00cc081ac3fd3b
ashokkumarga <ashokkumarga0@gmail.com>
2022-11-06 13:32:53 +0530

    new

---
+++ CALCULATOR/calci.js
@@ -0,0 +1,10 @@
+const display1 =document.querySelector('.display');
+const numbersElements =document.querySelectorAll('.number');
+const operationElements =document.querySelectorAll('.operation');
+const clearElement =document.querySelector('.all-clear');
+const dotElement =document.querySelector('.dot');
+const equalElement =document.querySelector('.equal');
+
+
+let dis1Num= "";
+let 
