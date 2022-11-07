f7b4338dd1f9e868091dacbd407d4b326c0d146f
ashokkumarga <ashokkumarga0@gmail.com>
2022-11-07 13:31:36 +0530

    cal

--- CALCULATOR/calci.js
+++ CALCULATOR/calci.js
@@ -1,10 +1,27 @@
-const display1 =document.querySelector('.display');
-const numbersElements =document.querySelectorAll('.number');
-const operationElements =document.querySelectorAll('.operation');
-const clearElement =document.querySelector('.all-clear');
-const dotElement =document.querySelector('.dot');
-const equalElement =document.querySelector('.equal');
+// const display1 =document.querySelector('.display');
+// const numbersElements =document.querySelectorAll('.number');
+// const operationElements =document.querySelectorAll('.operation');
+// const clearElement =document.querySelector('.all-clear');
+// const dotElement =document.querySelector('.dot');
+// const equalElement =document.querySelector('.equal');
 
 
-let dis1Num= "";
-let 
+// let dis1Num= "";
+// let 
+
+
+
+function view(num){
+    document.getElementById("result").button+=num;
+}
+function compute(){
+    var x=document.getElementById("result").button;
+    var y=eval(x);
+    document.getElementById("result").button=y;
+}
+function clr()
+{
+    document.getElementById("result").button="";
+}
+       
+        
